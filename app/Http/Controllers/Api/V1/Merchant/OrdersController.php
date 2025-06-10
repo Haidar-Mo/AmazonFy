00b1e285c668856b42348e81bbb9bfc0eddb09ca
itsMounir <mounirtoo.22@gmail.com>
2025-06-10 14:41:22 +0300

    fix a bug in accepting orders

--- app/Http/Controllers/Api/V1/Merchant/OrdersController.php
+++ app/Http/Controllers/Api/V1/Merchant/OrdersController.php
@@ -81,6 +81,13 @@ class OrdersController extends Controller
                 throw new AuthorizationException();
             }
             if ($request->accepted) {
+                $wallet = Auth::user()->wallet;
+                if ($wallet->available_balance < $shopOrder->total_price) {
+                    return $this->showMessage('You do not have enough balance', 400, false);
+                }
+                $wallet->available_balance -= $shopOrder->total_price;
+                $wallet->marginal_balance += $shopOrder->total_price;
+                $wallet->save();
                 $shopOrder->update(['status' => 'checking']);
             } else {
                 $shopOrder->update(['status', 'canceled']);
