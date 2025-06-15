82aafef204aa7d74f52a4ecb8f7cf9192ca3924f
Haidar Mohammad <it.eng.haidar@gmail.com>
2025-06-15 14:36:07 +0300

    dash: order

--- routes/V1/Dashboard/order.php
+++ routes/V1/Dashboard/order.php
@@ -8,12 +8,13 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('orders')
     ->middleware([
         'auth:sanctum',
-    'ability:' . TokenAbility::ACCESS_API->value,
+        'ability:' . TokenAbility::ACCESS_API->value,
     ])
     ->group(function () {
 
         Route::get('index', [OrderController::class, 'index']);
         Route::get('show/{id}', [OrderController::class, 'show']);
+        Route::post('create', [OrderController::class, 'store']);
         Route::post('update/{id}', [OrderController::class, 'update']);
         Route::post('cancel/{id}', [OrderController::class, 'cancel']);
 
