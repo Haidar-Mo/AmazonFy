ceaecc935906d230f8152aaaa1f10b8e70ee6e1e
Haidar Mohammad <it.eng.haidar@gmail.com>
2025-06-16 02:04:04 +0300

    CORS middileware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,9 +29,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-    
+
         $middleware->append([
-            \App\Http\Middleware\AcceptLanguage::class
+            \App\Http\Middleware\AcceptLanguage::class,
+            \App\Http\Middleware\CorsMiddleware::class,
+
         ]);
 
         $middleware->alias([
